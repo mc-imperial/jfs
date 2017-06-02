f290c86a2e4fa904e6b5b45c80ae8cc70d1162eb
Dan Liew <daniel.liew@imperial.ac.uk>
2017-06-02 16:53:04 +0100

    Teach AndHoistingPass to use new Z3 Node API

--- lib/Transform/AndHoistingPass.cpp
+++ lib/Transform/AndHoistingPass.cpp
@@ -19,7 +19,6 @@ namespace transform {
 
 bool AndHoistingPass::run(Query &q) {
   bool changed = false;
-  const JFSContext &ctx = q.getContext();
   std::vector<Z3ASTHandle> newConstraints;
   newConstraints.reserve(q.constraints.size());
   std::list<Z3ASTHandle> workList;
@@ -30,30 +29,20 @@ bool AndHoistingPass::run(Query &q) {
   while (workList.size() > 0) {
     Z3ASTHandle e = workList.front();
     workList.pop_front();
-
-    if (!Z3_is_app(ctx.z3Ctx, e)) {
-      // Not an application. Just add as a constraint
+    if (!e.isAppOf(Z3_OP_AND)) {
+      // Not an logical and application.
+      // Just add as a constraint
       newConstraints.push_back(e);
       continue;
     }
-
-    Z3AppHandle topLevelApp = Z3AppHandle(::Z3_to_app(ctx.z3Ctx, e), ctx.z3Ctx);
-    Z3FuncDeclHandle topLevelFunc =
-        Z3FuncDeclHandle(::Z3_get_app_decl(ctx.z3Ctx, topLevelApp), ctx.z3Ctx);
-    Z3_decl_kind kind = Z3_get_decl_kind(ctx.z3Ctx, topLevelFunc);
-    if (kind != Z3_OP_AND) {
-      // Not an and. Just add as a constraint
-      newConstraints.push_back(e);
-      continue;
-    }
-
     // This is an and expression. Push on to work list so we walk the AST.
     changed = true;
-    unsigned numArgs = Z3_get_app_num_args(ctx.z3Ctx, topLevelApp);
-    assert(numArgs >= 2 && "Unexpected number of args");
-    for (unsigned index = 0; index < numArgs; ++index) {
-      workList.push_front(Z3ASTHandle(
-          ::Z3_get_app_arg(ctx.z3Ctx, topLevelApp, index), ctx.z3Ctx));
+    assert(e.isApp() && "should be an application");
+    Z3AppHandle app = e.asApp();
+
+    assert(app.getNumKids() >= 2 && "Unexpected number of args");
+    for (unsigned index = 0; index < app.getNumKids(); ++index) {
+      workList.push_front(app.getKid(index));
     }
   }
 
