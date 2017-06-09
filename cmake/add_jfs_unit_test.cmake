58cd90ac4b9735bd34d4968a417dbc63a52f34ed
Dan Liew <daniel.liew@imperial.ac.uk>
2017-06-09 15:54:36 +0100

    Refactor CMake unit test code so we can reuse it in an external build.

---
+++ cmake/add_jfs_unit_test.cmake
@@ -0,0 +1,43 @@
+#===------------------------------------------------------------------------===#
+#
+#                         JFS - The JIT Fuzzing Solver
+#
+# Copyright 2017 Daniel Liew
+#
+# This file is distributed under the MIT license.
+# See LICENSE.txt for details.
+#
+#===------------------------------------------------------------------------===#
+set(GTEST_SRC_DIR "${CMAKE_SOURCE_DIR}/utils/googletest/googletest")
+
+# This keeps track of all the unit test
+# targets so we can ensure they are built
+# before trying to run them.
+define_property(GLOBAL
+  PROPERTY JFS_UNIT_TEST_TARGETS
+  BRIEF_DOCS "JFS unit tests"
+  FULL_DOCS "JFS unit tests"
+)
+
+set(GTEST_INCLUDE_DIR "${GTEST_SRC_DIR}/include")
+
+if (NOT IS_DIRECTORY "${GTEST_INCLUDE_DIR}")
+  message(FATAL_ERROR
+    "Cannot find GTest include directory \"${GTEST_INCLUDE_DIR}\"")
+endif()
+
+set (UNIT_TEST_EXE_SUFFIX "Test")
+function(add_jfs_unit_test target_name)
+  add_executable(${target_name}${UNIT_TEST_EXE_SUFFIX} ${ARGN})
+  target_link_libraries(${target_name}${UNIT_TEST_EXE_SUFFIX} PRIVATE jfs_gtest_main)
+  target_include_directories(${target_name}${UNIT_TEST_EXE_SUFFIX} BEFORE PRIVATE "${GTEST_INCLUDE_DIR}")
+  set_target_properties(${target_name}${UNIT_TEST_EXE_SUFFIX}
+    PROPERTIES
+    RUNTIME_OUTPUT_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+  )
+  set_property(GLOBAL
+    APPEND
+    PROPERTY JFS_UNIT_TEST_TARGETS
+    ${target_name}${UNIT_TEST_EXE_SUFFIX}
+  )
+endfunction()
