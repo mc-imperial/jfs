66cfbc2d5a66a3de5e71e51a8443023c257491df
Dan Liew <daniel.liew@imperial.ac.uk>
2017-07-04 21:14:12 +0100

    Unbreak builds where the generator is not "UNIX Makefiles"

--- cmake/jfs_external_project_utils.cmake
+++ cmake/jfs_external_project_utils.cmake
@@ -8,7 +8,7 @@
 # See LICENSE.txt for details.
 #
 #===------------------------------------------------------------------------===#
-function(jfs_get_external_project_build_command OUTPUT_VAR)
+function(jfs_get_external_project_build_command OUTPUT_VAR BUILD_DIR)
   if ("${CMAKE_GENERATOR}" STREQUAL "Unix Makefiles")
     # HACK: This hack means that if the build generator for JFS
     # is Make then `-j` value will passed through to make run
@@ -22,7 +22,7 @@ function(jfs_get_external_project_build_command OUTPUT_VAR)
     ProcessorCount(NUM_CPUS)
     message(STATUS "Detected ${NUM_CPUS} CPUS. Will run this many jobs when building runtime tests")
     set(JFS_EXTERNAL_PROJECT_BUILD_COMMAND
-      "BUILD_COMMAND" "${CMAKE_COMMAND}" "--build" "${RUNTIME_TEST_BUILD_DIR}" "--" "-j${NUM_CPUS}")
+      "BUILD_COMMAND" "${CMAKE_COMMAND}" "--build" "${BUILD_DIR}" "--" "-j${NUM_CPUS}")
   endif()
   set(${OUTPUT_VAR} ${JFS_EXTERNAL_PROJECT_BUILD_COMMAND} PARENT_SCOPE)
 endfunction()
