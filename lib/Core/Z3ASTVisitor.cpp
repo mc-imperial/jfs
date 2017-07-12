70a1dfe8097f8792c320f99de18914baa8379874
Dan Liew <daniel.liew@imperial.ac.uk>
2017-07-12 12:17:43 +0100

    Handle Z3's bvurem_i. This is a Z3 specific operator that until recently (see https://github.com/Z3Prover/z3/issues/1132 ) wasn't documented.

--- lib/Core/Z3ASTVisitor.cpp
+++ lib/Core/Z3ASTVisitor.cpp
@@ -75,6 +75,8 @@ void Z3ASTVisitor::visit(Z3ASTHandle e) {
     ACTION(visitBvUDiv(asApp))
   case Z3_OP_BSREM:
     ACTION(visitBvSRem(asApp))
+  case Z3_OP_BUREM_I:
+    LLVM_FALLTHROUGH;
   case Z3_OP_BUREM:
     ACTION(visitBvURem(asApp))
   case Z3_OP_BSMOD:
