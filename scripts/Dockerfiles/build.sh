3b17321f6bbc3e4f6576b78137a344aa86fa938b
Dan Liew <daniel.liew@imperial.ac.uk>
2017-08-15 22:45:55 +0100

    Fix typo

--- scripts/Dockerfiles/build.sh
+++ scripts/Dockerfiles/build.sh
@@ -10,9 +10,6 @@ FINAL_TAG="jfs_build:ubuntu1604"
 # Build base that we can use for other tools
 docker build -t "${BASE_TAG}" - < "${SCRIPT_DIR}/jfs_base_ubuntu_16.04.Dockerfile"
 
-docker build --build-arg BASE_IMAGE="${BASE_TAG}" -t "${FINAL_TAG}" -f "${SCRIPT_DIR}/jfs_build_ubuntu_16.04.Dockerfile" "${ROOT_DIR}"
-
-# TODO: Should squash image
 DOCKER_MAJOR_VERSION=$(docker info --format '{{.ServerVersion}}' | sed 's/^\([0-9]\+\)\.\([0-9]\+\).*$/\1/')
 DOCKER_MINOR_VERSION=$(docker info --format '{{.ServerVersion}}' | sed 's/^\([0-9]\+\)\.\([0-9]\+\).*$/\2/')
 DOCKER_BUILD_FILE="${SCRIPT_DIR}/jfs_build_ubuntu_16.04.Dockerfile"
@@ -40,3 +37,5 @@ docker build \
   -f "${DOCKER_BUILD_FILE}" \
   "${BUILD_OPTS[@]}" \
   "${ROOT_DIR}"
+
+# TODO: Should squash image
