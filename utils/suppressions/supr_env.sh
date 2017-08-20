56d1904da3b1ebc4b522a4d8f4fd8ee3cb81240c
Dan Liew <daniel.liew@imperial.ac.uk>
2017-08-20 15:26:44 +0100

    Add useful script for setting up suppression environment variables

---
+++ utils/suppressions/supr_env.sh
@@ -0,0 +1,11 @@
+# This file should be sourced to update
+# the environment with sanitizer suppressions
+set -x
+SUPPRESSIONS_ROOT_DIR="$(cd "${BASH_SOURCE[0]%/*}" && pwd )"
+if [ -d "${SUPPRESSIONS_ROOT_DIR}" ]; then
+  export LSAN_OPTIONS="suppressions=${SUPPRESSIONS_ROOT_DIR}/lsan/lsan_sup.txt"
+  export UBSAN_OPTIONS="suppressions=${SUPPRESSIONS_ROOT_DIR}/ubsan/ubsan_sup.txt"
+else
+ echo "SUPPRESSIONS_ROOT_DIR \"${SUPPRESSIONS_ROOT_DIR}\" is invalid"
+fi
+set +x
