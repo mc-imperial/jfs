01795156f492203fd1725b9316b0706760782310
Dan Liew <daniel.liew@imperial.ac.uk>
2017-08-27 21:57:21 +0100

    Add test for Float32/Float64 SMTLIB equality

---
+++ runtime/SMTLIB/SMTLIB/unittests/Float/Native/SMTLIBEquals.cpp
@@ -0,0 +1,43 @@
+//===----------------------------------------------------------------------===//
+//
+//                        JFS - The JIT Fuzzing Solver
+//
+// Copyright 2017 Daniel Liew
+//
+// This file is distributed under the MIT license.
+// See LICENSE.txt for details.
+//
+//===----------------------------------------------------------------------===//
+#include "SMTLIB/Float.h"
+#include "gtest/gtest.h"
+
+namespace {
+
+template <typename T> void checkDisjointConstantsDisjoint() {
+  T values[] = {T::getPositiveZero(),
+                T::getNegativeZero(),
+                T::getPositiveInfinity(),
+                T::getNegativeInfinity(),
+                T::getNaN(),
+                T(1.5),
+                T(-1.5)};
+  for (unsigned index = 0; index < sizeof(values) / sizeof(T); ++index) {
+    for (unsigned indexOther = 0; indexOther < sizeof(values) / sizeof(T);
+         ++indexOther) {
+      if (index == indexOther) {
+        ASSERT_TRUE(values[index] == values[indexOther]);
+      } else {
+        ASSERT_FALSE(values[index] == values[indexOther]);
+      }
+    }
+  }
+}
+}
+
+TEST(SMTLIBEquals, DisjointConstantsDisjointFloat32) {
+  checkDisjointConstantsDisjoint<Float32>();
+}
+
+TEST(SMTLIBEquals, DisjointConstantsDisjointFloat64) {
+  checkDisjointConstantsDisjoint<Float64>();
+}
