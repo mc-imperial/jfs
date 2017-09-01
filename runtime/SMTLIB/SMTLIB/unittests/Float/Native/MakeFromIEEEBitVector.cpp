4b02d46b8177906edd76e9a975a44711fe7f1c04
Dan Liew <daniel.liew@imperial.ac.uk>
2017-09-01 22:29:36 +0100

    Try to unbreak the Docker build

--- runtime/SMTLIB/SMTLIB/unittests/Float/Native/MakeFromIEEEBitVector.cpp
+++ runtime/SMTLIB/SMTLIB/unittests/Float/Native/MakeFromIEEEBitVector.cpp
@@ -15,7 +15,7 @@
 TEST(MakeFromIEEEBitVector, PositiveZeroFloat32) {
   Float32 f(BitVector<32>(0x0));
   ASSERT_EQ(f.getRawData(), 0.0f);
-  ASSERT_FALSE(signbit(f.getRawData()));
+  ASSERT_FALSE(std::signbit(f.getRawData()));
   Float32 zero = Float32::getPositiveZero();
   ASSERT_EQ(zero, f);
 }
@@ -23,7 +23,7 @@ TEST(MakeFromIEEEBitVector, PositiveZeroFloat32) {
 TEST(MakeFromIEEEBitVector, PositiveZeroFloat64) {
   Float64 f(BitVector<64>(0x0));
   ASSERT_EQ(f.getRawData(), 0.0);
-  ASSERT_FALSE(signbit(f.getRawData()));
+  ASSERT_FALSE(std::signbit(f.getRawData()));
   Float64 zero = Float64::getPositiveZero();
   ASSERT_EQ(zero, f);
 }
@@ -31,7 +31,7 @@ TEST(MakeFromIEEEBitVector, PositiveZeroFloat64) {
 TEST(MakeFromIEEEBitVector, NegativeZeroFloat32) {
   Float32 f(BitVector<32>(UINT32_C(0x80000000)));
   ASSERT_EQ(f.getRawData(), 0.0f);
-  ASSERT_TRUE(signbit(f.getRawData()));
+  ASSERT_TRUE(std::signbit(f.getRawData()));
   Float32 zero = Float32::getNegativeZero();
   ASSERT_EQ(zero, f);
 }
@@ -39,53 +39,53 @@ TEST(MakeFromIEEEBitVector, NegativeZeroFloat32) {
 TEST(MakeFromIEEEBitVector, NegativeZeroFloat64) {
   Float64 f(BitVector<64>(UINT64_C(0x8000000000000000)));
   ASSERT_EQ(f.getRawData(), 0.0);
-  ASSERT_TRUE(signbit(f.getRawData()));
+  ASSERT_TRUE(std::signbit(f.getRawData()));
   Float64 zero = Float64::getNegativeZero();
   ASSERT_EQ(zero, f);
 }
 
 TEST(MakeFromIEEEBitVector, PositiveInfinityFloat32) {
   Float32 f(BitVector<32>(UINT32_C(0x7f800000)));
-  ASSERT_TRUE(isinf(f.getRawData()));
-  ASSERT_FALSE(signbit(f.getRawData()));
+  ASSERT_TRUE(std::isinf(f.getRawData()));
+  ASSERT_FALSE(std::signbit(f.getRawData()));
   Float32 inf = Float32::getPositiveInfinity();
   ASSERT_EQ(inf, f);
 }
 
 TEST(MakeFromIEEEBitVector, PositiveInfinityFloat64) {
   Float64 f(BitVector<64>(UINT64_C(0x7ff0000000000000)));
-  ASSERT_TRUE(isinf(f.getRawData()));
-  ASSERT_FALSE(signbit(f.getRawData()));
+  ASSERT_TRUE(std::isinf(f.getRawData()));
+  ASSERT_FALSE(std::signbit(f.getRawData()));
   Float64 inf = Float64::getPositiveInfinity();
   ASSERT_EQ(inf, f);
 }
 
 TEST(MakeFromIEEEBitVector, NegativeInfinityFloat32) {
   Float32 f(BitVector<32>(UINT32_C(0xff800000)));
-  ASSERT_TRUE(isinf(f.getRawData()));
-  ASSERT_TRUE(signbit(f.getRawData()));
+  ASSERT_TRUE(std::isinf(f.getRawData()));
+  ASSERT_TRUE(std::signbit(f.getRawData()));
   Float32 inf = Float32::getNegativeInfinity();
   ASSERT_EQ(inf, f);
 }
 
 TEST(MakeFromIEEEBitVector, NegativeInfinityFloat64) {
   Float64 f(BitVector<64>(UINT64_C(0xfff0000000000000)));
-  ASSERT_TRUE(isinf(f.getRawData()));
-  ASSERT_TRUE(signbit(f.getRawData()));
+  ASSERT_TRUE(std::isinf(f.getRawData()));
+  ASSERT_TRUE(std::signbit(f.getRawData()));
   Float64 inf = Float64::getNegativeInfinity();
   ASSERT_EQ(inf, f);
 }
 
 TEST(MakeFromIEEEBitVector, NaNFloat32) {
   Float32 f(BitVector<32>(jfs_nr_float32_get_raw_bits(NAN)));
-  ASSERT_TRUE(isnan(f.getRawData()));
+  ASSERT_TRUE(std::isnan(f.getRawData()));
   auto NaN = Float32::getNaN();
   ASSERT_EQ(NaN, f);
 }
 
 TEST(MakeFromIEEEBitVector, NaNFloat64) {
   Float64 f(BitVector<64>(jfs_nr_float64_get_raw_bits(NAN)));
-  ASSERT_TRUE(isnan(f.getRawData()));
+  ASSERT_TRUE(std::isnan(f.getRawData()));
   auto NaN = Float64::getNaN();
   ASSERT_EQ(NaN, f);
 }
--- runtime/SMTLIB/SMTLIB/unittests/Float/Native/SpecialConstants.cpp
+++ runtime/SMTLIB/SMTLIB/unittests/Float/Native/SpecialConstants.cpp
@@ -15,57 +15,57 @@
 TEST(SpecialConstants, PositiveZeroFloat32) {
   Float32 zero = Float32::getPositiveZero();
   ASSERT_EQ(zero.getRawData(), 0.0f);
-  ASSERT_FALSE(signbit(zero.getRawData()));
+  ASSERT_FALSE(std::signbit(zero.getRawData()));
 }
 
 TEST(SpecialConstants, PositiveZeroFloat64) {
   Float64 zero = Float64::getPositiveZero();
   ASSERT_EQ(zero.getRawData(), 0.0);
-  ASSERT_FALSE(signbit(zero.getRawData()));
+  ASSERT_FALSE(std::signbit(zero.getRawData()));
 }
 
 TEST(SpecialConstants, NegativeZeroFloat32) {
   Float32 zero = Float32::getNegativeZero();
   ASSERT_EQ(zero.getRawData(), 0.0f);
-  ASSERT_TRUE(signbit(zero.getRawData()));
+  ASSERT_TRUE(std::signbit(zero.getRawData()));
 }
 
 TEST(SpecialConstants, NegativeZeroFloat64) {
   Float64 zero = Float64::getNegativeZero();
   ASSERT_EQ(zero.getRawData(), 0.0);
-  ASSERT_TRUE(signbit(zero.getRawData()));
+  ASSERT_TRUE(std::signbit(zero.getRawData()));
 }
 
 TEST(SpecialConstants, PositiveInfinityFloat32) {
   Float32 infinity = Float32::getPositiveInfinity();
-  ASSERT_TRUE(isinf(infinity.getRawData()));
-  ASSERT_FALSE(signbit(infinity.getRawData()));
+  ASSERT_TRUE(std::isinf(infinity.getRawData()));
+  ASSERT_FALSE(std::signbit(infinity.getRawData()));
 }
 
 TEST(SpecialConstants, PositiveInfinityFloat64) {
   Float64 infinity = Float64::getPositiveInfinity();
-  ASSERT_TRUE(isinf(infinity.getRawData()));
-  ASSERT_FALSE(signbit(infinity.getRawData()));
+  ASSERT_TRUE(std::isinf(infinity.getRawData()));
+  ASSERT_FALSE(std::signbit(infinity.getRawData()));
 }
 
 TEST(SpecialConstants, NegativeInfinityFloat32) {
   Float32 infinity = Float32::getNegativeInfinity();
-  ASSERT_TRUE(isinf(infinity.getRawData()));
-  ASSERT_TRUE(signbit(infinity.getRawData()));
+  ASSERT_TRUE(std::isinf(infinity.getRawData()));
+  ASSERT_TRUE(std::signbit(infinity.getRawData()));
 }
 
 TEST(SpecialConstants, NegativeInfinityFloat64) {
   Float64 infinity = Float64::getNegativeInfinity();
-  ASSERT_TRUE(isinf(infinity.getRawData()));
-  ASSERT_TRUE(signbit(infinity.getRawData()));
+  ASSERT_TRUE(std::isinf(infinity.getRawData()));
+  ASSERT_TRUE(std::signbit(infinity.getRawData()));
 }
 
 TEST(SpecialConstants, NaNFloat32) {
   Float32 num = Float32::getNaN();
-  ASSERT_TRUE(isnan(num.getRawData()));
+  ASSERT_TRUE(std::isnan(num.getRawData()));
 }
 
 TEST(SpecialConstants, NaNFloat64) {
   Float64 num = Float64::getNaN();
-  ASSERT_TRUE(isnan(num.getRawData()));
+  ASSERT_TRUE(std::isnan(num.getRawData()));
 }
