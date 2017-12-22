17cd2eb0876572057b8996036cc1abecd9c1cb68
Dan Liew <daniel.liew@imperial.ac.uk>
2017-12-22 15:14:58 +0000

    Fix race condition in ScopedTimerImpl that would cause a bounded deadlock.
    If ScopedTimerImpl destructor is called before the waiting thread
    has managed to initialise (i.e. start waiting on the condition variable)
    then when the notification is sent it will get missed and so the
    thread calling the ScopedTimerImpl destructor will deadlock.
    
    This is a bounded deadlock because the waiting thread waits for
    a finite amount of time.
    
    The fix is basically to spin in the ScopedTimerImpl destructor until
    the waiting thread has started waiting. We might take a performance
    hit for doing this but at least it fixes the deadlock.

--- lib/Support/ScopedTimer.cpp
+++ lib/Support/ScopedTimer.cpp
@@ -26,10 +26,12 @@ private:
   bool realWakeUp;
   std::chrono::time_point<std::chrono::steady_clock> startTime;
   std::chrono::time_point<std::chrono::steady_clock> endTime;
+  bool waiterStarted;
 
 public:
   void waiterFunction() {
     std::unique_lock<std::mutex> lock(cvMutex);
+    waiterStarted = true;
     // Loop just in case there are spurious wake ups
     while (true) {
       std::cv_status status = cv.wait_until(lock, endTime);
@@ -45,7 +47,7 @@ public:
   }
   ScopedTimerImpl(uint64_t maxTime, ScopedTimer::CallBackTy callBack)
       : maxTime(maxTime), callBack(callBack), waiter(nullptr),
-        realWakeUp(false) {
+        realWakeUp(false), waiterStarted(false) {
     startTime = std::chrono::steady_clock::now();
     std::chrono::seconds timeout(maxTime);
     endTime = startTime + timeout;
@@ -56,16 +58,27 @@ public:
     waiter.reset(new std::thread(&ScopedTimerImpl::waiterFunction, this));
   }
   ~ScopedTimerImpl() {
-    if (waiter) {
-      {
-        std::lock_guard<std::mutex> lock(cvMutex);
+    if (waiter == nullptr)
+      return;
+
+    // Spin until `waiterFunction()` is waiting.  If we don't do this we might
+    // call `notify_one()` before `waiterFunction()` is waiting and thus the
+    // signal will be lost resulting in a deadlock when `waiter->join()` is
+    // called.
+    //
+    // Given `waiterFunction()`'s implementation if `waiterStarted` is true
+    // and we managed to grab the mutex then `waiter` thread must be waiting.
+    while (true) {
+      std::lock_guard<std::mutex> lock(cvMutex);
+      if (waiterStarted) {
         // Wake up the thread if it's still running
         realWakeUp = true;
+        break;
       }
-      cv.notify_one();
-      if (waiter->joinable())
-        waiter->join();
     }
+    cv.notify_one();
+    if (waiter->joinable())
+      waiter->join();
   }
 
   uint64_t getRemainingTime() const {
