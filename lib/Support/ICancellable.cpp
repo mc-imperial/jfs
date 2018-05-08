eac9f0841beb3f59a9635649e422744c4d78fdd6
Ally Donaldson <afd@imperial.ac.uk>
2018-05-08 22:39:33 +0100

    Add missing `lib/Support/ICancellable.cpp` file that was supposed to be in 2f9ad0a6e2408622c7fb9f847c748fa114f28f4b but was missed out.
    Issue spotted by @jryans.

---
+++ lib/Support/ICancellable.cpp
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+//                        JFS - The JIT Fuzzing Solver
+//
+// Copyright 2017-2018 Daniel Liew
+//
+// This file is distributed under the MIT license.
+// See LICENSE.txt for details.
+//
+//===----------------------------------------------------------------------===//
+#include "jfs/Support/ICancellable.h"
+
+namespace jfs {
+namespace support {
+
+ICancellable::~ICancellable() {}
+
+} // namespace support
+} // namespace jfs
