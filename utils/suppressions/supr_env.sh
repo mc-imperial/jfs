a2ebfdfb725b5eecdeee1def7b31fe6607b0ca72
Ally Donaldson <afd@imperial.ac.uk>
2018-08-14 21:33:58 +0100

    Add UBSan libstdc++ pointer alginment suppression and also halt on error by default in the environment set by `supr_env.sh`.

--- utils/suppressions/supr_env.sh
+++ utils/suppressions/supr_env.sh
@@ -4,7 +4,7 @@ set -x
 SUPPRESSIONS_ROOT_DIR="$(cd "${BASH_SOURCE[0]%/*}" && pwd )"
 if [ -d "${SUPPRESSIONS_ROOT_DIR}" ]; then
   export LSAN_OPTIONS="suppressions=${SUPPRESSIONS_ROOT_DIR}/lsan/lsan_sup.txt"
-  export UBSAN_OPTIONS="suppressions=${SUPPRESSIONS_ROOT_DIR}/ubsan/ubsan_sup.txt"
+  export UBSAN_OPTIONS="suppressions=${SUPPRESSIONS_ROOT_DIR}/ubsan/ubsan_sup.txt,halt_on_error=1"
 else
  echo "SUPPRESSIONS_ROOT_DIR \"${SUPPRESSIONS_ROOT_DIR}\" is invalid"
 fi
